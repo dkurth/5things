a1d66874afbccf41b61e6496f550343ccd6a73be
Derek Kurth <dkurth@gmail.com>
2018-04-14 12:19:36 -0500

    css fix

--- views/html_head.php
+++ views/html_head.php
@@ -4,7 +4,7 @@
     <title>5 Things Generator</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?php $basePath?>/static/css/fivethings.css">
+    <link rel="stylesheet" href="<?php echo $basePath?>/static/css/fivethings.css">
     <script src="<?php echo $basePath?>/static/js/fivethings.js"></script>
 </head>
 <body>
