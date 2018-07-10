c118f639b486157091290a9b3cf97db9664a5a94
Derek Kurth <dkurth@gmail.com>
2018-07-09 20:50:20 -0500

    Use the correct article for replacement items.

--- src/FiveThings/ReplacementItem.php
+++ src/FiveThings/ReplacementItem.php
@@ -6,10 +6,12 @@ class ReplacementItem {
     public $id;
     public $name;
     public $type;
+    public $article;
 
     public function __construct(array $data) {
         $this->id = $data['Id'] ?? null;
-        $this->type = $data['Type'] ?? "thing";
+        $this->type = $data['Type'] ?? 'thing';
         $this->name = $data['Name'] ?? null;
+        $this->article = $data['Article'] ?? '';
     }
 }
